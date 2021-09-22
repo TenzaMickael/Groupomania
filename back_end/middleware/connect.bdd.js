91571ce94b2f52e8e8cd8b78307d260ecdb018fa
tenza mickael <mickaeltenza.42@gmail.com>
2021-09-22 21:50:45 +0200

    update back_end

--- back_end/middleware/connect.bdd.js
+++ back_end/middleware/connect.bdd.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
+    user: 'micka',
     password: '@Vivresesreves190886@',
     database: 'groupomania'
     
